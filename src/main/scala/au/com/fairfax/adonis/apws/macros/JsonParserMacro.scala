dbb551f2f13573ec5ac26d2b8f0366feaeb5dd6a
Amy Wong <amy.wong@fairfaxmedia.com.au>
2014-06-19 17:07:40 +1000

    covered cases of all primitive values

--- src/main/scala/au/com/fairfax/adonis/apws/macros/JsonParserMacro.scala
+++ src/main/scala/au/com/fairfax/adonis/apws/macros/JsonParserMacro.scala
@@ -4,7 +4,7 @@ import play.api.libs.json._
 import scala.language.experimental.macros
 
 import scala.reflect.macros.Context
-import scala.reflect.runtime.universe
+import scala.reflect.runtime.universe._
 
 object JsonParserMacro {
   type ParserType = JsValue => Any
@@ -13,6 +13,17 @@ object JsonParserMacro {
     import c.universe._
 
     val rootJsValue = "rootJsValue"
+
+    lazy val numTypes = List(typeOf[Double], typeOf[Float], typeOf[Short], typeOf[Int], typeOf[Long]) map typeToString
+
+    def typeToString(tpe: c.universe.Type): String = tpe.toString
+
+    def numQuote(jsValueVar: String)(fieldName: String)(typeStr: String) =
+      q"""
+          {
+            (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsNumber].value.${TermName("to" + typeStr)}
+          }
+      """
 
     def subExpr(tpe: c.universe.Type)(jsValueVar: String)(fieldName: String): c.universe.Tree = {
       //      println(s"tpe = $tpe, jsValueVar = $jsValueVar, fieldName = $fieldName")
@@ -46,12 +57,22 @@ object JsonParserMacro {
           """
 
         case _ =>
-          println(s"tpe.dealias = ${tpe.dealias}")
-          q"""
-              {
-                (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsNumber].value.toDouble
-              }
-          """
+          val typeStr = tpe.toString
+          typeStr match {
+            case t: String if numTypes contains t => numQuote(jsValueVar)(fieldName)(typeStr)
+            case t: String if t == typeToString(typeOf[Boolean]) =>
+              q"""
+                  {
+                    (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsBoolean].value
+                  }
+              """
+            case _ =>
+              q"""
+                  {
+                    (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsString].value
+                  }
+              """
+          }
       }
     }
 
@@ -64,9 +85,11 @@ object JsonParserMacro {
           parse _
       """
 
-    println( s"""result =
+    println(
+      s"""result =
            $result
        """.stripMargin)
+
     c.Expr[ParserType](result)
   }
 
