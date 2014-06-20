5605aa1d643295d790792f21d081f965cbe62820
Amy Wong <amy.wong@fairfaxmedia.com.au>
2014-06-20 19:24:34 +1000

    add parsing collection

--- src/main/scala/au/com/fairfax/adonis/apws/macros/JsonParserMacro.scala
+++ src/main/scala/au/com/fairfax/adonis/apws/macros/JsonParserMacro.scala
@@ -16,16 +16,32 @@ object JsonParserMacro {
 
     lazy val numTypes = List(typeOf[Double], typeOf[Float], typeOf[Short], typeOf[Int], typeOf[Long])
 
+    def parseCollectionQuote(tpe: c.universe.Type)(collType: String) = {
+      val quote = q"""
+        def parseCollection(jsArray: JsArray) = {
+          def createItem(item: JsValue) = {
+            ${subExpr(tpe)("item")("")}
+          }
+          jsArray.value.map(createItem).to[${TypeName(collType)}]
+        }
+      """
+      quote
+    }
+
+    def extractFieldString(jsValueVar: String)(fieldName: String) =
+      if (fieldName == "")
+        q"${TermName(jsValueVar)}"
+      else
+        q"(${TermName(jsValueVar)} \ $fieldName)"
+
     def numQuote(jsValueVar: String)(fieldName: String)(typeStr: String) =
       q"""
           {
-            (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsNumber].value.${TermName("to" + typeStr)}
+            ${extractFieldString(jsValueVar)(fieldName)}.asInstanceOf[JsNumber].value.${TermName("to" + typeStr)}
           }
       """
 
     def subExpr(tpe: c.universe.Type)(jsValueVar: String)(fieldName: String): c.universe.Tree = {
-      //      println(s"tpe = $tpe, jsValueVar = $jsValueVar, fieldName = $fieldName")
-
       val accessors = (tpe.declarations collect {
         case acc: MethodSymbol if acc.isCaseAccessor => acc
       }).toList
@@ -36,20 +52,16 @@ object JsonParserMacro {
             c.error(c.enclosingPosition, "Cannot materialize JsonParser for non-case class")
             return q"null"
           }
-
           val newJsValueVar = s"${jsValueVar}_$fieldName"
-
           val constrArgs = accessors map {
             accessor =>
               val fieldName = accessor.name.toString
               val fieldTpe = accessor.returnType.substituteTypes(tpe.typeConstructor.typeParams, tpe.typeArgs)
               subExpr(fieldTpe)(newJsValueVar)(fieldName)
           }
-          //          println(s"constrArgs = $constrArgs")
-
           q"""
             {
-              val ${TermName(newJsValueVar)} = (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsObject]
+              val ${TermName(newJsValueVar)} = ${extractFieldString(jsValueVar)(fieldName)}.asInstanceOf[JsObject]
               new $tpe(..$constrArgs)
             }
           """
@@ -60,21 +72,23 @@ object JsonParserMacro {
             case t: Type if t == typeOf[Boolean] =>
               q"""
                   {
-                    (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsBoolean].value
+                    ${extractFieldString(jsValueVar)(fieldName)}.asInstanceOf[JsBoolean].value
                   }
               """
             case t: Type if t == typeOf[String] =>
               q"""
                   {
-                    (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsString].value
+                    ${extractFieldString(jsValueVar)(fieldName)}.asInstanceOf[JsString].value
                   }
               """
-//            case t: Type if t.typeSymbol.asClass.getClass == classOf[List[_]].getName =>
-//              q"""
-//                  {
-//                    (${TermName(jsValueVar)} \ $fieldName).asInstanceOf[JsArray].value
-//                  }
-//              """
+            case t: Type if t.typeSymbol.asClass.fullName == typeOf[List[_]].typeSymbol.asClass.fullName =>
+              assert(t.typeArgs.size == 1)
+              q"""
+                  {
+                    ${parseCollectionQuote(t.typeArgs.head)("List")}
+                    parseCollection(${extractFieldString(jsValueVar)(fieldName)}.asInstanceOf[JsArray])
+                  }
+              """
           }
       }
     }
@@ -95,40 +109,10 @@ object JsonParserMacro {
 
     c.Expr[ParserType](result)
   }
-
-  def testMaterialize[T: c.WeakTypeTag](c: Context): c.Expr[ParserType] = {
-    import c.universe._
-
-    val tpe = weakTypeOf[T]
-
-    println(s"tpe = $tpe, tpe.typeSymbol.asClass.getName = ${tpe.typeSymbol.asClass.fullName}")
-    println(s"tpe.typeArgs = ${tpe.typeArgs}")
-
-    val accessors = (tpe.declarations collect {
-      case acc: MethodSymbol if acc.isCaseAccessor => acc
-    }).toList
-
-    val accessor = accessors.head
-    println(s"fieldName = ${accessor.name.toString}")
-    val accessorType = accessor.returnType
-    println(s"accessorType without type params substitution = ${accessorType}")
-    val subsitutedAccType = accessorType.substituteTypes(tpe.typeConstructor.typeParams, tpe.typeArgs)
-    println(s"accessorType with type params substitution = $subsitutedAccType")
-
-    val result =
-      q"""
-          import play.api.libs.json._
-          def parser(jsValue: JsValue): Any = "testing"
-          parser _
-      """
-    c.Expr[ParserType](result)
-  }
 }
 
 import JsonParserMacro._
 
 object JsonMaterializers {
   def jsonParserMacro[T]: ParserType = macro materializeJsonParser[T]
-
-  def test[T]: ParserType = macro testMaterialize[T]
 }
