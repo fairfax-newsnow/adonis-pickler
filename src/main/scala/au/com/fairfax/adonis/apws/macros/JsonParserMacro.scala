8602b6273d79095959ae072517007b5d6a800a9b
Amy Wong <amy.wong@fairfaxmedia.com.au>
2014-06-24 11:55:00 +1000

    use unambiguous variable names

--- src/main/scala/au/com/fairfax/adonis/apws/macros/JsonParserMacro.scala
+++ src/main/scala/au/com/fairfax/adonis/apws/macros/JsonParserMacro.scala
@@ -21,7 +21,7 @@ object JsonParserMacro {
   def materializeJsonParser[T: c.WeakTypeTag](c: Context): c.Expr[JsonParser[T]] = {
     import c.universe._
 
-    // don't declare return type after def ${TermName("create" + simplified)}(item: JsValue), o.w. will get meaningless error of type ... not found in macro call
+    // don't declare return type after def ${TermName(createItemMeth)}(item: P), o.w. will get meaningless error of type ... not found in macro call
     def createItemQuote(tpe: c.universe.Type)(createItemMeth: String) =
       q"""
         def ${TermName(createItemMeth)}(item: P) =
@@ -29,23 +29,23 @@ object JsonParserMacro {
       """
 
     def parseCollectionQuote(tpe: c.universe.Type)(collType: String) = {
-      val create = createItemMeth(tpe.toString)
+      val createMeth = createItemMeth(tpe.toString)
       q"""
         def ${TermName(parseCollectionMeth)}(array: P) = {
-          ${createItemQuote(tpe)(create)}
+          ${createItemQuote(tpe)(createMeth)}
           val arraySize = reader.readArrayLength(array)
           (0 until arraySize).to[${TypeName(collType)}].map {
-            idx => ${TermName(create)}(reader.readArrayElem(array, idx))
+            idx => ${TermName(createMeth)}(reader.readArrayElem(array, idx))
           }
         }
       """
     }
 
     def parseMapQuote(keyTpe: c.universe.Type)(valTpe: c.universe.Type) = {
-      val List(createKey, createVal) = List(keyTpe, valTpe) map (t => createItemMeth(t.toString))
-      var createQuote = List(createItemQuote(keyTpe)(createKey))
+      val List(createKeyMeth, createValMeth) = List(keyTpe, valTpe) map (t => createItemMeth(t.toString))
+      var createQuote = List(createItemQuote(keyTpe)(createKeyMeth))
       if (keyTpe != valTpe)
-        createQuote = createItemQuote(valTpe)(createVal) :: createQuote
+        createQuote = createItemQuote(valTpe)(createValMeth) :: createQuote
       q"""
         def ${TermName(parseMapMeth)}(map: P) = {
           ..$createQuote
@@ -54,27 +54,25 @@ object JsonParserMacro {
             val tuple = reader.readArrayElem(map, idx)
             val key = reader.readArrayElem(tuple, 0)
             val value = reader.readArrayElem(tuple, 1)
-            ${TermName(createKey)}(key) -> ${TermName(createVal)}(value)
+            ${TermName(createKeyMeth)}(key) -> ${TermName(createValMeth)}(value)
           }.toMap
         }
       """
     }
 
-    def extractJsonField(jsValueVar: String)(fieldName: String) =
-      if (fieldName == "")
-        q"${TermName(jsValueVar)}"
+    def extractJsonField(jsonVarNm: String)(fieldNm: String) =
+      if (fieldNm == "")
+        q"${TermName(jsonVarNm)}"
       else
-        q"reader.readObjectField(${TermName(jsValueVar)}, $fieldName)"
+        q"reader.readObjectField(${TermName(jsonVarNm)}, $fieldNm)"
 
-    def readDouble(jsValueVar: String)(fieldName: String) =
-      q"reader.readNumber(${extractJsonField(jsValueVar)(fieldName)})"
+    def readDouble(jsonVarNm: String)(fieldNm: String) =
+      q"reader.readNumber(${extractJsonField(jsonVarNm)(fieldNm)})"
 
-    def subExpr(tpe: c.universe.Type)(jsValueVar: String)(fieldName: String): c.universe.Tree = {
+    def subExpr(tpe: c.universe.Type)(jsonVarNm: String)(fieldNm: String): c.universe.Tree = {
       val accessors = (tpe.decls collect {
         case acc: MethodSymbol if acc.isCaseAccessor => acc
       }).toList
-
-      val tpeSymClass: Type => String = _.typeSymbol.asClass.fullName
 
       accessors match {
         case x :: _ =>
@@ -82,7 +80,7 @@ object JsonParserMacro {
             c.error(c.enclosingPosition, "Cannot materialize JsonParser for non-case class")
             return q"null"
           }
-          val newJsValueVar = s"${jsValueVar}_$fieldName"
+          val newJsValueVar = s"${jsonVarNm}_$fieldNm"
           val constrArgs = accessors map {
             accessor =>
               val fieldName = accessor.name.toString
@@ -90,29 +88,30 @@ object JsonParserMacro {
               subExpr(fieldTpe)(newJsValueVar)(fieldName)
           }
           q"""
-            val ${TermName(newJsValueVar)} = ${extractJsonField(jsValueVar)(fieldName)}
+            val ${TermName(newJsValueVar)} = ${extractJsonField(jsonVarNm)(fieldNm)}
             new $tpe(..$constrArgs)
           """
 
         case _ =>
+          lazy val tpeSymClass: Type => String = _.typeSymbol.asClass.fullName
           tpe match {
-            case t: Type if t == typeOf[Double] => readDouble(jsValueVar)(fieldName)
-            case t: Type if t == typeOf[Float] => q"${readDouble(jsValueVar)(fieldName)}.asInstanceOf[Float]"
-            case t: Type if t == typeOf[Short] => q"${readDouble(jsValueVar)(fieldName)}.asInstanceOf[Short]"
-            case t: Type if t == typeOf[Int] => q"${readDouble(jsValueVar)(fieldName)}.asInstanceOf[Int]"
-            case t: Type if t == typeOf[Long] => q"${readDouble(jsValueVar)(fieldName)}.asInstanceOf[Long]"
-            case t: Type if t == typeOf[Boolean] => q"reader.readBoolean(${extractJsonField(jsValueVar)(fieldName)})"
-            case t: Type if t == typeOf[String] => q"reader.readString(${extractJsonField(jsValueVar)(fieldName)})"
+            case t: Type if t == typeOf[Double] => readDouble(jsonVarNm)(fieldNm)
+            case t: Type if t == typeOf[Float] => q"${readDouble(jsonVarNm)(fieldNm)}.asInstanceOf[Float]"
+            case t: Type if t == typeOf[Short] => q"${readDouble(jsonVarNm)(fieldNm)}.asInstanceOf[Short]"
+            case t: Type if t == typeOf[Int] => q"${readDouble(jsonVarNm)(fieldNm)}.asInstanceOf[Int]"
+            case t: Type if t == typeOf[Long] => q"${readDouble(jsonVarNm)(fieldNm)}.asInstanceOf[Long]"
+            case t: Type if t == typeOf[Boolean] => q"reader.readBoolean(${extractJsonField(jsonVarNm)(fieldNm)})"
+            case t: Type if t == typeOf[String] => q"reader.readString(${extractJsonField(jsonVarNm)(fieldNm)})"
             case t: Type if tpeSymClass(t) == tpeSymClass(typeOf[List[_]]) =>
               q"""
                 ${parseCollectionQuote(t.typeArgs.head)("List")}
-                ${TermName(parseCollectionMeth)}(${extractJsonField(jsValueVar)(fieldName)})
+                ${TermName(parseCollectionMeth)}(${extractJsonField(jsonVarNm)(fieldNm)})
               """
             case t: Type if tpeSymClass(t) == tpeSymClass(typeOf[Map[_, _]]) =>
               val List(key, value) = t.typeArgs
               q"""
                 ${parseMapQuote(key)(value)}
-                ${TermName(parseMapMeth)}(${extractJsonField(jsValueVar)(fieldName)})
+                ${TermName(parseMapMeth)}(${extractJsonField(jsonVarNm)(fieldNm)})
               """
           }
       }
@@ -139,8 +138,9 @@ object JsonParserMacro {
   }
 }
 
-import JsonParserMacro._
-
 object JsonMaterializers {
+
+  import JsonParserMacro._
+
   def jsonParserMacro[T]: JsonParser[T] = macro materializeJsonParser[T]
 }
