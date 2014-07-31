f18c4bca605a7f4fd5b34ce4cd8cf687dadcbe3b
Daniel Le Clere <dleclere@fairfaxmedia.com.au>
2014-07-31 20:46:12 +1000

    Modified pickler to support registration of types with generics.

--- src/main/scala/au/com/fairfax/adonis/apws/base/JsSerialisable.scala
+++ src/main/scala/au/com/fairfax/adonis/apws/base/JsSerialisable.scala
@@ -1,11 +1,11 @@
 package au.com.fairfax.adonis.apws.base
 
-import au.com.fairfax.adonis.apws.macros.{JReader, JBuilder}
+import au.com.fairfax.adonis.apws.macros.{TypeProvider, JBuilder}
 
 /**
  * @author Daniel Le Clere
  */
-trait JsSerialisable {
+trait JsSerialisable extends TypeProvider {
   def format[J](implicit builder: JBuilder[J]): J
 }
 
--- src/main/scala/au/com/fairfax/adonis/apws/macros/JsonRegistry.scala
+++ src/main/scala/au/com/fairfax/adonis/apws/macros/JsonRegistry.scala
@@ -1,19 +1,33 @@
 package au.com.fairfax.adonis.apws.macros
 
 import scala.collection.mutable.{HashMap => MHashMap}
-
+import scala.language.experimental.macros
 import scala.reflect.ClassTag
 import au.com.fairfax.adonis.utils.simpleTypeNm
+import scala.reflect.macros.blackbox.Context
+
+
+trait TypeKeyProvider[T] {
+  def key: String
+}
 
 object JsonRegistry extends BaseJsonRegistry
 
-trait JsonRegistry {
-  def format[J, T: ClassTag](obj: T)(implicit builder: JBuilder[J]): J
+trait TypeProvider {
+
+  implicit def materializeTypeKeyProvider[T]: TypeKeyProvider[T] =
+  macro TypeKeyProvider.materializeTypeKeyProvider[T]
+}
+
+trait JsonRegistry extends TypeProvider {
+
+  def format[J, T: ClassTag](obj: T)(implicit builder: JBuilder[J], keyProvider: TypeKeyProvider[T]): J
 
   def parse[J](json: J)(implicit reader: JReader[J]): Any
 }
 
 class BaseJsonRegistry extends JsonRegistry {
+
   private val parsers = new MHashMap[String, JsonParser[_]]
   private val formatters = new MHashMap[String, JsonFormatter[_]]
   lazy val strReplacement: String Map String =
@@ -30,18 +44,18 @@ class BaseJsonRegistry extends JsonRegistry {
     (z, x) => if (z == x._1) x._2 else z
   }.replace('$', '.')
 
-  def register[T: ClassTag](implicit parser: JsonParser[T], formatter: JsonFormatter[T]): Unit = {
-    val key = toMapKey(className[T])
+  def register[T](implicit parser: JsonParser[T], formatter: JsonFormatter[T], keyProvider: TypeKeyProvider[T]): Unit = {
+    val key = keyProvider.key
+    println("registering: " + key)
     parsers += (key -> parser)
     formatters += (key -> formatter)
-
-//    println(s"parsers.key = ${parsers.keys.toList}")
-//    println(s"formatters.key = ${formatters.keys.toList}")
   }
 
-  override def format[J, T: ClassTag](obj: T)(implicit builder: JBuilder[J]): J = {
-    val key = toMapKey(className[T])
-    formatters.get(key) match {
+  override def format[J, T: ClassTag](obj: T)(implicit builder: JBuilder[J], keyProvider: TypeKeyProvider[T]): J = {
+    val key = keyProvider.key
+    println("key: " + key)
+    println("className: " + toMapKey(className[T]))
+    formatters.get(if (key == "T") toMapKey(className[T]) else key) match {
       case Some(formatter) =>
         formatter format obj
 
@@ -58,8 +72,24 @@ class BaseJsonRegistry extends JsonRegistry {
         parser parse json
 
       case None =>
+        println("Registered parsers")
+        parsers.keys.foreach(println(_))
         throw new Error(s"No parser exists for $key")
     }
   }
 
+}
+
+object TypeKeyProvider {
+  def materializeTypeKeyProvider[T: c.WeakTypeTag](c: Context) = {
+    import c.universe._
+    val typeTag = c.universe.weakTypeOf[T]
+    println("Materialising: " + typeTag.toString)
+    q"""
+    val provider = new au.com.fairfax.adonis.apws.macros.TypeKeyProvider[$typeTag] {
+      def key: String = ${typeTag.toString()}
+    }
+    provider
+    """
+  }
 }
