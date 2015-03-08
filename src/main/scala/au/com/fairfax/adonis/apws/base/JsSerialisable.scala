18684550f341bd18e836d989e661bcc97c2145a5
Amy Wong <amy.wong@fairfaxmedia.com.au>
2015-03-09 10:58:35 +1100

    fix issue in integrating JsonAkkaSerializer, change JsSerialisable to extend java.io.Serializable

--- src/main/scala/au/com/fairfax/adonis/apws/base/JsSerialisable.scala
+++ src/main/scala/au/com/fairfax/adonis/apws/base/JsSerialisable.scala
@@ -5,7 +5,7 @@ import au.com.fairfax.adonis.apws.macros.{TypeProvider, JBuilder}
 /**
  * @author Daniel Le Clere
  */
-trait JsSerialisable extends TypeProvider {
+trait JsSerialisable extends TypeProvider with java.io.Serializable {
   def format[J](implicit builder: JBuilder[J]): J
 }
 
